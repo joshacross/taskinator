f373e001bae8f760d564f255cb3136f64beddf19
Killa J <joshuaalexandercross@gmail.com>
2021-03-06 14:03:25 -0600

    refactored code to get, create object package, and send as an argument

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,37 @@
 var formEl = document.querySelector("#task-form"); 
 var tasksToDoEl = document.querySelector("#tasks-to-do"); 
 
-var createTaskHandler = function(event) { 
+var taskFormHandler = function(event) { 
   event.preventDefault(); 
 
-  var listItemEl = document.createElement("li"); 
-  listItemEl.className = "task-item"; 
-  listItemEl.textContent = "This is a new task."; 
-  tasksToDoEl.appendChild(listItemEl); 
-  }; 
+  // find task-name and take-type from HTML
+  var taskNameInput = document.querySelector("input.value[name='task-name']");
+  var taskTypeInput = document.querySelector("select[name='task-type']").value;
 
-  formEl.addEventListener("submit", createTaskHandler);
+  // package up data as an object
+  var taskDataObj = {
+    name: taskNameInput,
+    type: taskTypeInput
+  };
+
+  // send object as an argument to createTaskEl
+  createTaskEl(taskDataObj);
+
+}; 
+
+var createTaskEl = function(taskDataObj) {
+  var listItemEl = document.createElement("li");
+  listItemEl.className = "task-item";
+
+  //create div to hold task info and add to list item
+  var taskInfoEl = document.createElement("div");
+  taskInfoEl.className = "task-info";
+  taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+
+  listItemEl.appendChild(taskInfoEl);
+
+  //add entirelist item to list
+  tasksToDoEl.appendChild(listItemEl);
+};
+
+  formEl.addEventListener("submit", taskFormHandler);

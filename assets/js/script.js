1696401274f57030fe36058c90d6356f5afa5f85
Killa J <joshuaalexandercross@gmail.com>
2021-03-08 22:48:13 -0600

    ability to uedit and update tasks and return form start

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,22 +17,51 @@ var taskFormHandler = function(event) {
     return false;
   }
 
-formEl.reset();
+  formEl.reset();
 
-// reset form fields for next task to be entered
-document.querySelector("input[name='task-name']").value;
-document.querySelector("select[name='task-type']").value;
+  // reset form fields for next task to be entered
+  document.querySelector("input[name='task-name']").value;
+  document.querySelector("select[name='task-type']").value;
 
-  // package up data as an object
-var taskDataObj = {
-  name: taskNameInput,
-  type: taskTypeInput
-};
+    // package up data as an object
+  var taskDataObj = {
+    name: taskNameInput,
+    type: taskTypeInput
+  };
 
-// send object as an argument to createTaskEl
-createTaskEl(taskDataObj);
+  var isEdit = formEl.hasAttribute("data-task-id");
+
+  // has data attribute, so get task id and call function to complete edit process
+  if (isEdit) {
+    var taskId = formEl.getAttribute("data-task-id");
+    completeEditTask(taskNameInput, taskTypeInput, taskId);
+  } 
+  // no data attribute, so create object as normal and pass to createTaskEl function
+  else {
+    var taskDataObj = {
+      name: taskNameInput,
+      type: taskTypeInput
+    };
+
+    createTaskEl(taskDataObj);
+  }
 
 }; 
+
+var completeEditTask = function(taskName, taskType, taskId) {
+  // find the matching task list item
+  var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
+
+  // set new values
+  taskSelected.querySelector("h3.task-name").textContent = taskName;
+  taskSelected.querySelector("span.task-type").textContent = taskType;
+
+  alert("Task Updated!");
+
+  formEl.removeAttribute("data-task-id");
+  document.querySelector("#save-task").textContent = "Add Task";
+
+};
 
 var createTaskEl = function(taskDataObj) {
   var listItemEl = document.createElement("li");
